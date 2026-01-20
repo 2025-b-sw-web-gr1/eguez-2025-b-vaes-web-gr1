9a2215c013d370432272f7948786ed7004213829
Adrian Eguez <adrian.eguez@epn.edu.ec>
2026-01-20 17:43:14 -0500

    feat(i18n): add internationalization support with English and Dutch translations
    - Created AppController to handle basic routes and i18n greetings.
    - Implemented AppService to return localized greetings.
    - Configured I18nModule in AppModule with language resolvers and loader options.
    - Added English and Dutch translation files for general messages and validation.
    - Set up main application entry point with global validation pipes and exception filters.
    - Added TypeScript configuration files for building the project.

--- 03 - server/poli/src/app.module.ts
+++ 03 - server/poli/src/app.module.ts
@@ -7,6 +7,10 @@ import { Ingrediente } from './ingredientes/ingrediente.entity';
 import { RecetasModule } from './recetas/recetas.module';
 import { IngredientesModule } from './ingredientes/ingredientes.module';
 import { AuthController } from './auth.controller';
+import { I18nModule, 
+  QueryResolver, AcceptLanguageResolver,
+HeaderResolver, I18nJsonLoader } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [ // dentro de imports van los módulos que use nuestra aplicación
@@ -18,6 +22,16 @@ import { AuthController } from './auth.controller';
       }), 
     RecetasModule, 
     IngredientesModule,
+        I18nModule.forRoot({
+      fallbackLanguage: 'es',
+      loader: I18nJsonLoader,
+      loaderOptions: { path: path.join(__dirname, '/i18n/'), watch: true },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] }, // ?lang=es
+        AcceptLanguageResolver,                   // header Accept-Language
+        new HeaderResolver(['x-lang']),           // header personalizado
+      ],
+    }),
   ],
   controllers: [
     AppController,
